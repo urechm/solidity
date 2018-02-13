1f8f7fd5a19155823f770c559246c0d96e9c5847
chriseth <chris@ethereum.org>
2018-02-13 11:54:22 +0100

    Run EndToEnd tests on circle, too.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -30,6 +30,20 @@ set -e
 
 REPO_ROOT="$(dirname "$0")"/..
 
+if [ "$1" -e --junit_report ]
+then
+    if [ -z "$2" ]
+    then
+        echo "Usage: $0 [--junit_report <report_directory>]"
+        exit 1
+    fi
+    testargs_no_opt="--logger=JUNIT,test_suite,$2/no_opt.xml"
+    testargs_opt="--logger=JUNIT,test_suite,$2/opt.xml"
+else
+    testargs_no_opt=''
+    testargs_opt=''
+fi
+
 echo "Running commandline tests..."
 "$REPO_ROOT/test/cmdlineTests.sh"
 
@@ -43,8 +57,8 @@ elif [ -z $CI ]; then
 else
     mkdir -p /tmp/test
     # Update hash below if binary is changed.
-    wget -q -O /tmp/test/eth https://github.com/ethereum/cpp-ethereum/releases/download/solidityTester/eth_byzantium2
-    test "$(shasum /tmp/test/eth)" = "4dc3f208475f622be7c8e53bee720e14cd254c6f  /tmp/test/eth"
+    wget -q -O /tmp/test/eth https://github.com/ethereum/cpp-ethereum/releases/download/solidityTester/eth_no_microhttpd
+    test "$(shasum /tmp/test/eth)" = "8e0e2c2b9f4cf4ad8956a81c1520bbb817768a53  /tmp/test/eth"
     sync
     chmod +x /tmp/test/eth
     sync # Otherwise we might get a "text file busy" error
@@ -68,9 +82,9 @@ sleep 2
 # And then run the Solidity unit-tests (once without optimization, once with),
 # pointing to that IPC endpoint.
 echo "--> Running tests without optimizer..."
-  "$REPO_ROOT"/build/test/soltest --show-progress -- --ipcpath /tmp/test/geth.ipc && \
+  "$REPO_ROOT"/build/test/soltest --show-progress $testargs_no_opt -- --ipcpath /tmp/test/geth.ipc && \
   echo "--> Running tests WITH optimizer..." && \
-  "$REPO_ROOT"/build/test/soltest --show-progress -- --optimize --ipcpath /tmp/test/geth.ipc
+  "$REPO_ROOT"/build/test/soltest --show-progress $testargs_opt -- --optimize --ipcpath /tmp/test/geth.ipc
 ERROR_CODE=$?
 pkill "$ETH_PID" || true
 sleep 4
